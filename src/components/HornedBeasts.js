a0f519f59184793ec8a221b851e376a133da21b3
shaheenaz <shaheenaz99@gmail.com>
2021-06-07 15:17:08 +0300

    solving the lab

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,16 +1,54 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardColumns from 'react-bootstrap/CardColumns'
+
 // import Cat from 'src/cat.jpg'
 
 class HornedBeasts extends React.Component{
+constructor(props){
+    super(props)
+    this.state = {
+        favorite : 0
+    }
+}
+
+numOffav=()=>(
+    this.setState({
+        favorite : this.state.favorite+1}
+    )
+)
+
+
+
+
 render(){
     return(
         <div>
-                <h2>{this.props.animalName}</h2>
+                {/* <h2>{this.props.animalName}</h2> */}
                 {/* <img src='https://cdn.britannica.com/22/206222-131-E921E1FB/Domestic-feline-tabby-cat.jpg' alt='NN' title="NN"/> */}
-                <img src={this.props.picture} alt="" title=""/>
-                <p>{this.props.desciption}</p>
+                {/* <img  onClick={this.numOffav}src={this.props.picture} alt="" title=""/>
+                <p> favorite 💟:{this.state.favorite}</p>
+                <p>{this.props.desciption}</p> */}
 
 
+
+
+<CardColumns>
+                <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" onClick={this.numOffav}src={this.props.picture} />
+  <Card.Body>
+    <Card.Title>{this.props.animalName}</Card.Title>
+    <Card.Text>
+    {this.props.desciption}
+    favorite 💟:{this.state.favorite}
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+</CardColumns>
 
 
         </div>
